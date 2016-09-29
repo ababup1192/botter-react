6311889895f290ba1d2cb58c86274dd5007a3ea2
ababup1192 <ababup1192@gmail.com>
2016-09-29 12:16:42 +0900

    Add localStorage

--- main.jsx
+++ main.jsx
@@ -2,23 +2,34 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { text: "", tweets: Immutable.List() };
+        // ローカルストレージの内容をstateの初期値として使う。
+        const storageTweets = Immutable.List(JSON.parse(localStorage.getItem("tweets")));
+        this.state = { text: "", tweets: storageTweets };
     }
 
+    // inputの内容が変わったら、textに内容を反映。
     handleChange(e) {
         var textValue = e.target.value;
         this.setState({ text: textValue, tweets: this.state.tweets });
     }
 
+    // keyが押されたとき。
     handleKeyDown(e) {
-        // エンターキーが押されたときの処理
+        // エンターキーが押され、かつ内容が空じゃなかったときの処理
         if (e.keyCode === 13 && this.state.text !== "") {
-            console.log(this.state.text);
-            this.setState({ text: "", tweets: this.state.tweets });
+            // stateのtweetsの末尾に今の内容を追加。
+            const newTweet = this.state.tweets.push(this.state.text);
+            // ストレージにも反映
+            localStorage.setItem("tweets", JSON.stringify(newTweet));
+            // stateに反映。
+            this.setState({ text: "", tweets: newTweet });
         }
     }
 
     render() {
+        // stateのtweetsをliのlistに変える。
+        const tweets = this.state.tweets.reverse().map((tweet) => <li>{tweet}</li>);
+
         return <div>
             <input type="text"
                 value={this.state.text}
@@ -26,9 +37,11 @@ class App extends React.Component {
                 onKeyDown={(e) => this.handleKeyDown(e) }
                 onChange={(e) => this.handleChange(e) }
                 />
-            <ul></ul>
+            <ul>{tweets}</ul>
         </div>;
     }
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
+// 消す魔法
+// localStorage.clear();
